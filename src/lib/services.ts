8fb075f5915803b74ce8815d0071ed415b269b94
Rogério Bonfim <rogeriomateus@icloud.com>
2026-03-18 23:55:07 -0300

    idv4

--- src/lib/services.ts
+++ src/lib/services.ts
@@ -1,14 +1,6 @@
+import { v4 as uuidv4 } from "uuid";
+
 export function generateId() {
-    if (typeof window !== "undefined" && window.crypto?.randomUUID) {
-        return window.crypto.randomUUID();
-    }
     // Fallback manual para contextos não-seguros (HTTP)
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
-        /[xy]/g,
-        function (c) {
-            const r = (Math.random() * 16) | 0;
-            const v = c === "x" ? r : (r & 0x3) | 0x8;
-            return v.toString(16);
-        },
-    );
+    return uuidv4();
 }
