c0eec08acd9c5c227486f447023f62ff019b7eed
Rogério Bonfim <rogeriomateus@icloud.com>
2026-03-18 22:16:52 -0300

    prisma config

--- telegram-user-service-nest/prisma.config.ts
+++ telegram-user-service-nest/prisma.config.ts
@@ -2,10 +2,10 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-    schema: "../prisma/schema.prisma",
+    schema: "prisma/schema.prisma",
     migrations: {
-        path: "../prisma/migrations",
-        seed: "npx tsx ../prisma/seed.ts",
+        path: "prisma/migrations",
+        seed: "npx tsx prisma/seed.ts",
     },
     datasource: {
         url: process.env["DATABASE_URL"],
